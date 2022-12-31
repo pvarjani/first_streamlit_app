7ccbcd2e0ddad05fd3028762c459ce65790410cc
Pankaj Varjani <pankajvarjani09@gmail.com>
2022-12-31 15:56:04 -0800

    Update streamlit_app.py
    adding stop at the end

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if streamlit.button('Get Fruit Load list'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
-streamlit.stop()
+
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
@@ -59,3 +59,4 @@ if streamlit.button('Add a Fruit to te list'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
+streamlit.stop()
